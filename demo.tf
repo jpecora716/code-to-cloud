651410967015386497ba32abb15aba5f513e8263
Jim Pecora <jpecora@paloaltonetworks.com>
2023-03-06 08:46:48 -0500

    lock ubuntu ami

--- demo.tf
+++ demo.tf
@@ -1,21 +1,5 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "codetocloud" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-07dc2dd8e0efbc46a"
   instance_type = "t3.micro"
   network_interface {
     network_interface_id = aws_network_interface.demo_nic.id
